3887dc09f62718cbc9bbdde8517333f879d9dcaf
Tarun Singh <tarunsingh5169202@gmail.com>
2020-04-27 18:33:41 +0530

    Updates

---
+++ precache-manifest.8bac11de43abe81d945a419904cb1d0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f37fb54c1e4886fbc5a94f7ca63445c",
+    "url": "/ImageSearch/index.html"
+  },
+  {
+    "revision": "e522096fa19fd5cfdd36",
+    "url": "/ImageSearch/static/css/main.91ddc2cb.chunk.css"
+  },
+  {
+    "revision": "2d4bd92103f9d0f3bcaf",
+    "url": "/ImageSearch/static/js/2.c4df6d31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ImageSearch/static/js/2.c4df6d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e522096fa19fd5cfdd36",
+    "url": "/ImageSearch/static/js/main.4cdd49ad.chunk.js"
+  },
+  {
+    "revision": "6194febad91944aeb9e4",
+    "url": "/ImageSearch/static/js/runtime-main.6743d194.js"
+  }
+]);
